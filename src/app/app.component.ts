b47f9013dd6eedd9032c99be6b10fafd80c53939
gnoose <jieli3838@gmail.com>
2021-06-22 15:53:05 -0700

    fix: issues on format  * only /[`*+=\[\]{}|]/ can pass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
   files: File[] = [];
   jsonObj: any;
   // format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  format = /[`*+=\[\]{}|<>]/;
+  format = /[`*+=\[\]{}|]/;
 
   constructor(
     private translatorService: TranslatorService
