8500b81d6351a4168172d7b375d2241dabbf34b7
gnoose <jieli3838@gmail.com>
2021-06-23 21:18:49 -0700

    fix: <ul><li></li></ul> replace to empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent {
   jsonObj: any;
   // format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
   format = /[`*+=\[\]{}|]/;
-  replaceList = ['&lt;ul&gt;', '&lt;li&gt;', '&lt;/li&gt;', '&lt;/ul&gt;', '&lt;br&gt;'];
+  replaceList = ['&lt;ul&gt;', '&lt;li&gt;', '&lt;/li&gt;', '&lt;/ul&gt;', '&lt;br&gt;', '<ul>', '</ul>', '</li>', '</ul>'];
 
   constructor(
     private translatorService: TranslatorService
